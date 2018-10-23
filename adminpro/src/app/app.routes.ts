aac5872b284340f7574319d6dea73499b44cd66c
Irving Herrera <irving.herreramolina@gmail.com>
2018-10-23 12:32:39 -0500

    se definio rutas hijas para agregar el login, componente principal

--- adminpro/src/app/app.routes.ts
+++ adminpro/src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { PagesComponent } from './pages/pages.component';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
@@ -5,13 +6,20 @@ import { ProgressComponent } from './pages/progress/progress.component';
 import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
+
 const appRoutes: Routes =  [
-    {path: 'dashboard', component: DashboardComponent},
+    {
+        path: '',
+        component: PagesComponent,
+        children: [
+            {path: 'dashboard', component: DashboardComponent},
+            {path: 'progress', component: ProgressComponent},
+            {path: 'grafica1', component: Graficas1Component},
+            {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+        ]
+    },
     {path: 'login', component: LoginComponent},
     {path: 'register', component: DashboardComponent},
-    {path: 'progress', component: ProgressComponent},
-    {path: 'grafica1', component: Graficas1Component},
-    {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
     {path: '**', component: NopagefoundComponent },
 ];
 
