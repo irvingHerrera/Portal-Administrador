5808e248c5bf26867a57406ebf951a855b49813b
Irving Herrera <irving.herreramolina@gmail.com>
2018-10-30 11:54:50 -0600

    se agrego class check al seleccionar el cambio de tema

--- adminpro/src/app/pages/accout-settings/accout-settings.component.ts
+++ adminpro/src/app/pages/accout-settings/accout-settings.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { inject } from '@angular/core/testing';
 import { DOCUMENT } from '@angular/platform-browser';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-accout-settings',
@@ -16,9 +17,21 @@ export class AccoutSettingsComponent implements OnInit {
   }
 
 
-  cambiarColor(tema: string) {
+  cambiarColor(tema: string, link: any) {
+    this.aplicarCheck(link);
     let url = `assets/css/colors/${ tema }.css`;
     this._document.getElementById('tema').setAttribute('href', url);
+  }
+
+  aplicarCheck(link: any) {
+    let selectores: any = document.getElementsByClassName('selector');
+
+    for( let elemento of selectores) {
+      elemento.classList.remove('working');
+    }
+
+    link.classList.add('working');
+
   }
 
 }
