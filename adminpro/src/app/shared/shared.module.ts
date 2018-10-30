c1bc479f4fb87fa00f97b4a82bf391c8fde2be97
Irving Herrera <irving.herreramolina@gmail.com>
2018-10-30 15:49:00 -0600

    creacion de menu sidebar con routerlink

--- adminpro/src/app/shared/shared.module.ts
+++ adminpro/src/app/shared/shared.module.ts
@@ -3,14 +3,19 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
+    imports: [
+        RouterModule,
+        CommonModule
+    ],
     declarations: [
         NopagefoundComponent,
         HeaderComponent,
         SidebarComponent,
-        BreadcrumbsComponent,
-        
+        BreadcrumbsComponent
     ],
     exports: [
         NopagefoundComponent,
