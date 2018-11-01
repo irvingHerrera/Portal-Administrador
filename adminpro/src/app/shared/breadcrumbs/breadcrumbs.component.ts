9e200e802af5c49858068721f6b3feed1dd42de4
Irving Herrera <irving.herreramolina@gmail.com>
2018-10-31 23:43:21 -0600

    añadir metadata al header de la pagina

--- adminpro/src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ adminpro/src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivationEnd } from '@angular/router';
 import { filter, map } from 'rxjs/operators';
-import { Title } from '@angular/platform-browser';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -13,10 +13,16 @@ export class BreadcrumbsComponent implements OnInit {
   titulo: string;
 
   constructor( private router: Router,
-               private title: Title) {
+               private title: Title,
+               private meta: Meta) {
     this.getDataRoute().subscribe( data => {
       this.titulo = data.titulo;
       this.title.setTitle(this.titulo);
+      const metaTag: MetaDefinition = {
+        name: 'Descripcion',
+        content: this.titulo };
+
+        this.meta.updateTag(metaTag);
     } );
   }
 
