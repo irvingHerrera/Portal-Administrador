780057bb80c52872da09cf25b4d102e46f81de2b
Irving Herrera <irving.herreramolina@gmail.com>
2018-10-31 23:35:49 -0600

    breacon, titulo de la pagina y titulo de la pestaña con el nombre

--- adminpro/src/app/pages/pages.routes.ts
+++ adminpro/src/app/pages/pages.routes.ts
@@ -13,12 +13,12 @@ const pagesRoutes: Routes = [
         path: '',
         component: PagesComponent,
         children: [
-            {path: 'dashboard', component: DashboardComponent},
-            {path: 'progress', component: ProgressComponent},
-            {path: 'grafica1', component: Graficas1Component},
-            {path: 'promesas', component: PromesasComponent},
-            {path: 'rxjs', component: RxjsComponent},
-            {path: 'account-settings', component: AccoutSettingsComponent},
+            {path: 'dashboard', component: DashboardComponent, data: { titulo: 'Dashboard' } },
+            {path: 'progress', component: ProgressComponent, data: { titulo: 'Progress' } },
+            {path: 'grafica1', component: Graficas1Component, data: { titulo: 'Gráfica' } },
+            {path: 'promesas', component: PromesasComponent,  data: { titulo: 'Promesas' } },
+            {path: 'rxjs', component: RxjsComponent,  data: { titulo: 'RxJs' }},
+            {path: 'account-settings', component: AccoutSettingsComponent,  data: { titulo: 'Ajustes del Tema' }},
             {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
         ]
     }
