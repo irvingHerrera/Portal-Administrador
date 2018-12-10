926fda12674c06134e5efdbba119adb0d8cc8d2c
Irving Herrera <irving.herreramolina@gmail.com>
2018-12-10 15:42:02 -0600

    se creo servicio de usuario

--- adminpro/src/app/login/register.component.ts
+++ adminpro/src/app/login/register.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
+import swal from 'sweetalert';
+import { UsuarioService } from '../services/service.index';
+
 declare function init_plugins();
 
 @Component({
@@ -12,7 +15,26 @@ export class RegisterComponent implements OnInit {
 
   forma: FormGroup;
 
-  constructor() { }
+  constructor(
+    public _usuarioService: UsuarioService
+  ) { }
+
+  sonIguales( campo1: string, campo2: string ) {
+    return ( group: FormGroup ) => {
+
+      let pass1 = group.controls[campo1].value;
+      let pass2 = group.controls[campo2].value;
+
+      if ( pass1 === pass2 ){
+        return null;
+      }
+
+      return {
+        sonIguales: true
+      };
+
+    };
+  }
 
   ngOnInit() {
    init_plugins();
@@ -24,11 +46,28 @@ export class RegisterComponent implements OnInit {
     password: new FormControl(null, Validators.required ),
     password2: new FormControl(null, Validators.required ),
     condiciones: new FormControl( false ),
+   }, { validators: this.sonIguales( 'password', 'password2') });
 
+   this.forma.setValue({
+      nombre: 'Test ',
+      correo: 'test@test.com',
+      password: '123',
+      password2: '1234',
+      condiciones: true
    });
+
   }
 
   registrarUsuario() {
+
+    if ( this.forma.invalid ) {
+      return;
+    }
+
+    if ( !this.forma.value.condiciones ) {
+      swal('Importante', 'Debe de aceptar las condiciones', 'warning');
+      return;
+    }
     console.log( this.forma.value );
   }
 
--- adminpro/src/app/services/service.module.ts
+++ adminpro/src/app/services/service.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { SettingsService, SidebarService, SharedService  } from "./service.index";
+import { SettingsService, SidebarService, SharedService, UsuarioService  } from './service.index';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  providers: [SettingsService, SidebarService, SharedService],
+  providers: [SettingsService, SidebarService, SharedService, UsuarioService],
   declarations: []
 })
 export class ServiceModule { }
