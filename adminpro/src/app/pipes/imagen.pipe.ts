692019e8781fbce61bf64e6d18f1ff6026efea4c
Irving Herrera <irving.herreramolina@gmail.com>
2018-12-25 22:54:04 -0600

    implementado pipe de imagen, imagen perfil

--- adminpro/src/app/pipes/imagen.pipe.ts
+++ adminpro/src/app/pipes/imagen.pipe.ts
@@ -1,12 +1,41 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { URL_SERVICIOS } from '../config/config';
 
 @Pipe({
   name: 'imagen'
 })
 export class ImagenPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return 'TEST';
+  transform( img: string, tipo: string = 'usuario'): any {
+
+    let url = URL_SERVICIOS + '/img';
+    console.log(img, 'img');
+    if ( !img ) {
+      console.log(url + '/usuarios/xxx');
+      return url + '/usuarios/xxx';
+    }
+
+    if ( img.indexOf('https') >= 0 ) {
+      return img;
+    }
+
+    switch ( tipo ) {
+      case 'usuario':
+        url += '/usuarios/' + img;
+        break;
+      case 'medico':
+        url += '/medicos/' + img;
+        break;
+      case 'hospital':
+        url += '/hospitales/' + img;
+        break;
+      default:
+        console.log('Tipo de imagen no existe, usuario, meciso, hospital');
+        url += '/usuarios/xxx';
+        break;
+    }
+
+    return url;
   }
 
 }
